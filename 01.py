202750d8b551358a8f0cf97da1839d92e98b54a5
python_study <harry1080@163.com>
2018-03-24 11:09:49 +0800

    Update 01.py

--- 01.py
+++ 01.py
@@ -2,7 +2,7 @@
 # @Author: Marte
 # @Date:   2018-03-24 10:25:35
 # @Last Modified by:   Marte
-# @Last Modified time: 2018-03-24 10:43:24
+# @Last Modified time: 2018-03-24 11:09:39
 # 判断回文数和素数小程序
 import sys
 
@@ -10,26 +10,40 @@ import sys
 # 在num处定义需要判断的数字
 num = 131
 
-num_p = 0
-num_t = num
 
-is_palin = False
-is_prime = False
 
-while num_t !=0:
-    num_p = num_p * 10 + num_t % 10
-    num_t =num_t / 10
+# is_palin = False
+# is_prime = False
 
-if num == num_p:
-    is_palin = True
 
-for i in range(2,num):
-    if num % i ==0:
-        break
+'''
+    回文数的求解
+'''
+
+def is_palin(num):
+    num_p = 0
+    num_t = num
+    while num_t !=0:
+        num_p = num_p * 10 + num_t % 10
+        num_t =num_t / 10
+    if num == num_p:
+        return True
     else:
-        is_prime = True
+        return False
+
+'''
+    素数求解
+'''
+
+def is_prime(num):
+    for i in range(2,num):
+        if num % i ==0:
+           return False
+    return True
+
+
 def main():
-    if is_palin and is_prime:
+    if is_palin(num) and is_prime(num):
         print 'OK'
     else:
         print 'NO'
